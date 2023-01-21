9df5066fa4b8793c496099ec92818f929e9c8cf1
Tetiana Omelchenko <t.omel4enko@gmail.com>
2023-01-21 17:45:33 +0800

    Add TagListView

--- todo/urls.py
+++ todo/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from todo_app.views import TagListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("tags/", TagListView.as_view(), name="tags-list"),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo_app.models import Tag
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
