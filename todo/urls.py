ef7926def10314cddbcd438bb0c7869d8c15e9a9
Tetiana Omelchenko <t.omel4enko@gmail.com>
2023-01-21 17:57:38 +0800

    Add Tag create, update and delete views

--- todo/urls.py
+++ todo/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from todo_app.views import TagListView
+from todo_app.views import TagListView, TagCreateView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("tags/", TagListView.as_view(), name="tags-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo_app.models import Tag
@@ -6,4 +7,21 @@ from todo_app.models import Tag
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tags-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tags-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("tags-list")
 
